17cbf7b61add778b10e3ddd1ac41564851de166d
P4anix <pancok_1@yahoo.it>
2023-05-02 21:57:33 +0200

    card senza thumb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,28 @@
 </div>
 
 
+<section>
+    <div class="btn-top">
+        <button>
+            <h3>CURRENT SERIES</h3>
+        </button>
+    </div>
+    <div class="locandine">
+        
+        @foreach ($comics as $item)
+        <div class="prova">
+        <img src="{{ $item['thumb'] }}" alt="Immagine">
+        <h3>{{ $item['title'] }}</h3>
+        </div>
+        @endforeach
+        
+    </div>
+    <div class="btn-bottom">
+        <button>
+            <h4>LOAD MORE</h4>
+        </button>
+    </div>
+</section>
 
-<div class="card">
-    @foreach ($comics as $item)
-    <h3>{{ $item['title'] }}</h3>
-    <img src="{{ $item['thumb'] }}" alt="Immagine">
-    @endforeach
-
-</div>
 
 @endsection
