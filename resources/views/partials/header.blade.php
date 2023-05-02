24e5be54059fe670e8a293455e0703d6e33484e3
P4anix <pancok_1@yahoo.it>
2023-05-02 18:20:53 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo" srcset="">
+        </div>
+        <div class="lista">
+            <ul>
+                @foreach ($links as $link)
+                <li><strong><a href="#">{{ $link }}</a></strong></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</header>
